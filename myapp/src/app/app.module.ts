80e9364e4616e3d24dba620dbe7e340055ada8d9
marwen <marwenkhorchani7@gmail.com>
2020-12-17 22:06:54 +0100

    member home page

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {APP_BASE_HREF} from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { MemberComponent } from './member/member.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     NavbarComponent,
     LoginComponent,
+    MemberComponent,
     
   ],
   imports: [
